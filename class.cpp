e933bc455f3042997dc64439fe95254632ccae09
malikrafsan <pro.malikakbar2357@gmail.com>
2021-06-02 21:38:24 +0700

    add comment in class.cpp

--- class.cpp
+++ class.cpp
@@ -2,76 +2,102 @@
 #include "class.hpp"
 
 Vehicle::Vehicle(int x, int y) {
+    // Defining absis and ordinat attribute
     absis = x;
     ordinat = y;
 }
 
-Vehicle::~Vehicle() { 
+Vehicle::~Vehicle() {
     std::cout<<"         ,--.!,  " <<std::endl;
     std::cout<<"      __/   -*-  " <<std::endl;
     std::cout<<"    ,d08b.  '|`  " <<std::endl;
     std::cout<<"    0088MM      "<<std::endl;
     std::cout<<"    `9MMP'      " <<std::endl;
 
+    // Case if vehicle type is Flying Car
     if (vehicleType == "Flying Car") {
         std::cout << "Actually we have to bomb your vehicle again because the car part is still survive >:(\n" << std::endl;
-    } else {
+    } // Other type case 
+    else {
         std::cout<< "Your " << vehicleType << " has been blown up :D" << std::endl;
     }
 }
 
 void Vehicle::setSpeed (){
+    // Check whether Engine is already on
     if (!isEngineOn) {
         std::cout << "The engine is not turned on yet" << std::endl;
         return;
     }
 
+    // DICTIONARY
     double newSpeed;
+
+    // ALGORITHM
+    // Asking input new speed
     std::cout << "New speed: "; std::cin >> newSpeed;
+
+    // Invalid input
     if (newSpeed < 0) {
         std::cout << "Speed cannot be under 0" << std::endl;
     } else if (newSpeed > maxSpeed) {
         std::cout << "Your speed cannot exceed speed limit: " << maxSpeed << std::endl;
-    } else {
+    } 
+    // Valid input 
+    else {
         speed = newSpeed;
         std::cout << "Speed has been changed to " << newSpeed << std::endl;
     }
 }
 
 void Vehicle::turnOnEngine(){
+    // Check whether Engine is already on
     if (isEngineOn){
         std::cout << "Engine is already on" << std::endl;
         return;
     }
+    // Assigning value to true
     isEngineOn = true;
     std::cout<<"Engine is turned on" << std::endl;
 }
 
 void Vehicle::move() {
+    // Check whether Engine is already on
     if (!isEngineOn) {
         std::cout << "The engine is not turned on yet" << std::endl;
         return;
     }
 
+    // Check whether speed is 0
     if (speed == 0) {
         std::cout << "Please set your speed > 0 first" << std::endl;
         return;
     }
 
+    // DICTIONARY
     int x, y;
+
+    // ALGORITHM
+    // Asking input from user
     std::cout << "Please input how much you wanna go" << std::endl;
     std::cout << "X direction: "; std::cin >> x;
     std::cout << "Y direction: "; std::cin >> y;
 
+    // Take absolute value
     int moveX = x, moveY = y;
     if (x < 0) { moveX = -x; }
     if (y < 0) { moveY = -y; }
+
+    // Calculate loss fuel
     double loss = lossFuel(moveX,moveY); 
     
+    // Not enough fuel case
     if (loss > fuelCapacity){
         std::cout << "Not enough fuel" << std::endl;
         return;
-    } else {
+    } 
+    // Valid case 
+    else {
         absis += x;
         ordinat += y;
         fuelCapacity -= loss;
@@ -80,15 +106,21 @@ void Vehicle::move() {
 }
 
 void Vehicle::addPassanger() {
+    // ALGORITHM
+    // Valid case
     if (passanger < maxSeats){
         passanger += 1;
         std::cout << "Passanger has been added" << std::endl;
-    } else {
+    } 
+    // number of passanger = maximum seats case
+    else {
         std::cout << "Seats are full" << std::endl;
     }
 }
 
 void Vehicle::printCommand() {
+    // ALGORITHM
+    // Display basic commands that can be performed
     std::cout << "> " << vehicleType << " Commands" << std::endl;
     std::cout << "1. Turn on engine" << std::endl;
     std::cout << "2. Set speed" << std::endl;
@@ -100,6 +132,8 @@ void Vehicle::printCommand() {
 }
 
 void Vehicle::printInformation() {
+    // ALGORITHM
+    // Display basic informations of the vehicle
     std::cout << "\n> Information <" << std::endl;
     std::cout << "1. Engine is " << (isEngineOn ? "on" : "off") <<std::endl;
     std::cout << "2. Speed: " << speed <<std::endl;
@@ -109,6 +143,8 @@ void Vehicle::printInformation() {
 }
 
 void Vehicle::printStats() {
+    // ALGORITHM
+    // Display basic statistics of the vehicle
     std::cout << "> " << vehicleType << " Statistics <" << std::endl;
     std::cout << "1. LossFuel : " << lossFuelFormula << std::endl;
     std::cout << "2. Maximum number of seats : 2" << std::endl;
@@ -117,6 +153,8 @@ void Vehicle::printStats() {
 }
 
 void Vehicle::refuel() {
+    // ALGORITHM
+    // Assigning fuel capacity to maximum fuel
     fuelCapacity = maxFuel;
     std::cout << "Your " << vehicleType << " has been refueled" << std::endl;
 }
@@ -124,23 +162,32 @@ void Vehicle::refuel() {
 // =========================================================================================
 
 LandVehicle::LandVehicle(int x,int y): Vehicle(x,y) {}
+// Constructing LandVehicle object from Vehicle class
 
 // =========================================================================================
 
 Motorcycle::Motorcycle(int x, int y): LandVehicle(x,y) {
+    // Construct Motocycle object from LandVehicle class
+
+    // DICTIONARY
     char YN;
+
+    // ALGORITHM
+    // Asking user input
     std::cout << "Do you want your motorcycle to have helm storage? (Y/N) "; std::cin >> YN;
     while (YN != 'Y' and YN != 'N'){
         wrongInput();
         std::cout << "Do you want your motorcycle to have helm storage? (Y/N) "; std::cin >> YN;
     }
+    hasHelmStorage = (YN == 'Y' ? true : false);
 
+    // Display ASCII art
     std::cout << "\n" << std::endl;
     std::cout<< "   ,_o     " << std::endl;
     std::cout<< ".-/c-/,::  " << std::endl;
     std::cout<< "(_)'==(_)  " << std::endl;
 
-    hasHelmStorage = (YN == 'Y' ? true : false);
+    // Contructor message
     std::cout<< "Your motorcycle in (" << absis << "," << ordinat << ") ";
     if (hasHelmStorage) {
         std::cout << "with helm storage";
@@ -149,6 +196,7 @@ Motorcycle::Motorcycle(int x, int y): LandVehicle(x,y) {
     }
     std::cout << " has been magically created :D" <<std::endl;
 
+    // Define attributes' value
     number_of_wheels = 2;
     maxSeats = 2;
     maxSpeed = 10;
@@ -158,6 +206,8 @@ Motorcycle::Motorcycle(int x, int y): LandVehicle(x,y) {
 }
 
 void Motorcycle::addExternalHelmStorage(){
+    // ALGORITHM
+    // Check whether the motorcycle already has helm storage
     if (hasHelmStorage){
         std::cout << "Already has helm storage" << std::endl;
     } else {
@@ -167,23 +217,36 @@ void Motorcycle::addExternalHelmStorage(){
 }
 
 double Motorcycle::lossFuel(int moveX, int moveY) {
+    // ALGORITHM
+    // Return loss fuel calculation of Motorcycle class
     return 0.5*(speed*moveX + speed*moveY)*passanger;
 }
 
 void Motorcycle::printCommand(){
+    // ALGORITHM
+    // Call printCommand procedure from Vehicle class
     Vehicle::printCommand();
+    
+    // Display additional command from Motorcycle class
     std::cout << "8. Add external helm storage" << std::endl;
     std::cout << "0. Stop riding\n" << std::endl;
     std::cout << "Input yout choice: ";
 }
 
 void Motorcycle::printInformation() {
+    // ALGORITHM
+    // Call printInformation procedure from Vehicle class
     Vehicle::printInformation();
+
+    // Display additional information from Motorcycle class
     std::cout << "6. Has helm storage: " << (hasHelmStorage ? "yes" : "no") <<std::endl;
 }
 
 void Motorcycle::addPassanger() {
+    // ALGORITHM
+    // Check whether the motorcycle has helm storage
     if (hasHelmStorage) {
+        // Call addPassanger procedure from Vehicle class
         Vehicle::addPassanger();
     } else {
         std::cout << "Please add helm storage before add passanger" << std::endl;
@@ -193,20 +256,27 @@ void Motorcycle::addPassanger() {
 // =========================================================================================
 
 Car::Car(int x, int y): LandVehicle(x,y) {
+    // Construct Car object from LandVehicle class
+
+    //ALGORITHM
+    // Asking user input
     std::cout << "How many number of seats of your car: "; std::cin >> maxSeats;
     while (maxSeats < 1) {
         std::cout << "Your input is invalid, number of seats must be > 1" << std::endl;
         std::cout << "How many number of seats of your car: "; std::cin >> maxSeats;
     }
 
+    // Display ASCII art
     std::cout << "\n" << std::endl;
     std::cout << "   ______         " << std::endl;
     std::cout << "  /|_||_\\`.__     " << std::endl;
     std::cout << " (   _    _ _\\    " << std::endl;
     std::cout << " =`-(_)--(_)-'    " << std::endl;
 
+    // Display constructor message
     std::cout<< "Your car in (" << absis << "," << ordinat << ") with " << maxSeats << " number of seats has been magically created :D" << std::endl;
     
+    // Define attributes' value
     number_of_wheels = 4;
     maxSpeed = 20;
     maxFuel = 2000;
@@ -215,17 +285,25 @@ Car::Car(int x, int y): LandVehicle(x,y) {
 }
 
 double Car::lossFuel(int moveX, int moveY) { 
+    // ALGORITHM
+    // Return loss fuel calculation of Car class
     return (speed * (1.5) * (moveX + moveY) * (0.75 * passanger)); 
 }
 
 void Car::printCommand() {
+    // ALGORITHM
+    // Call printCommand() procedure from Vehicle class
     Vehicle::printCommand();
+
+    // Display additional command from Car class
     std::cout << "8. Use NOS (increase max speed)" << std::endl;
     std::cout << "0. Stop driving\n" << std::endl;
     std::cout << "Input yout choice: ";
 }
 
 void Car::useNOS() {
+    // ALGORITHM
+    // Check whether the Car is already using NOS
     if (usingNOS) {
         std::cout << "You are already using NOS" << std::endl;
     } else {
@@ -237,38 +315,61 @@ void Car::useNOS() {
 // =========================================================================================
 
 FlyingVehicle::FlyingVehicle(int x, int y): Vehicle(x,y) {}
+// Constructing FlyingVehicle object from Vehicle class
 
 void FlyingVehicle::printCommand() {
+    // ALGORITHM
+    // Call printCommand() from Vehicle class
     Vehicle::printCommand();
+
+    // Display additional command from FlyingVehicle class
     std::cout << "8. Set your flying height" << std::endl;
 }
 
 void FlyingVehicle::printInformation() {
+    // ALGORITHM
+    // Call printInformation procedure from Vehicle class
     Vehicle::printInformation();
+
+    // Display additional information from FlyingVehicle class
     std::cout << "7. Altitude: " << altitude << std::endl;
 }
 
 void FlyingVehicle::move() {
+    // ALGORITHM
+    // Check whether Plane altitude is already > 0
     if (altitude <= 0) {
         std::cout << "Your altitude must be > 0 first to move" << std::endl;
     } else {
+        // Call move procedure from Vehicle class
         Vehicle::move();
     }
 }
 
 void FlyingVehicle::printStats() {
+    // ALGORITHM
+    // Call printStats procedure from Vehicle class
     Vehicle::printStats();
+
+    // Display additional statistic from FlyingVehicle class
     std::cout << "5. Maximum altitude: " << maxAlti << std::endl;
 }
 
 void FlyingVehicle::setAltitude() {
+    // DICTIONARY
     int newAltitude;
+
+    // ALGORITHM
+    // Asking user input
     std::cout << "New altitude: "; std::cin >> newAltitude;
 
+    // Valid case
     if (newAltitude >= 0 && newAltitude <= maxAlti) {
         altitude = newAltitude;
         std::cout << "Your current altitude is " << newAltitude << std::endl;
-    } else {
+    } 
+    // Invalid case 
+    else {
         std::cout << vehicleType << " altitude must be >= 0 and <= " << maxAlti << std::endl;
     }
 }
@@ -276,27 +377,37 @@ void FlyingVehicle::setAltitude() {
 // =========================================================================================
 
 Plane::Plane(int x, int y): FlyingVehicle(x,y) {
+    // Construct Plane object from FlyingVehicle class
+
+    // DICTIONARY
     int choice;
 
+    // ALGORITHM
     std::cout << "Please choose your plane" << std::endl;
     std::cout << "1. Boeing (number of seats: 7.000, max fuel capacity: 80.000)" << std::endl;
     std::cout << "2. Airbus (number of seats: 10.000, max fuel capacity: 100.000)" << std::endl;
     std::cout << "3. Customize (number of seats & max fuel capacity are customizable)" << std::endl;
     
     while (true) {
+        // Asking user input
         std::cout << "Input your choice: "; std::cin >> choice;
         
+        // Case 1
         if (choice == 1) {
             PlaneType = "Boeing";
             maxSeats = 7000;
             maxFuel = 80000;
             break;
-        } else if (choice == 2) {
+        } 
+        // Case 2
+        else if (choice == 2) {
             PlaneType = "Airbus";
             maxSeats = 10000;
             maxFuel = 100000;
             break;
-        } else if (choice == 3) {
+        } 
+        // Case 3
+        else if (choice == 3) {
             std::cout << "Input your plane type: "; std::cin >> PlaneType;
             std::cout << "Input your plane number of seats: "; std::cin >> maxSeats;
             while (maxSeats < 1) {
@@ -309,12 +420,15 @@ Plane::Plane(int x, int y): FlyingVehicle(x,y) {
                 std::cout << "Input your plane maximum fuel capacity: "; std::cin >> maxSeats;
             }
             break;
-        } else {
+        } 
+        // Default case
+        else {
             wrongInput();
         }
     }
 
     while (true) {
+        // Asking user input
         std::cout << "Please input your number of engines: "; std::cin >> number_of_engine;
         if (number_of_engine <= 1) {
             std::cout << "Number of engine must be > 1\n" << std::endl;
@@ -324,13 +438,16 @@ Plane::Plane(int x, int y): FlyingVehicle(x,y) {
         }
     }
 
+    // Display ASCII art
     std::cout << "\n" << std::endl;
     std::cout << "       __|__          " << std::endl;
     std::cout << "--------(_)--------   " << std::endl;
     std::cout << "  O  O       O  O     " << std::endl;
 
+    // Display constructor message
     std::cout << "\nYour " << PlaneType << " plane with " << number_of_engine << " number of engines and " << maxSeats << " number of seats has been magically created :D" << std::endl;
 
+    // Define attributes' value
     maxAlti = 20000;
     maxSpeed = 100;
     lossFuelFormula = "2 * speed * (moveX + moveY) * (0.5 * passanger) * ((25.000 - altitude) / 10.000)";
@@ -338,26 +455,41 @@ Plane::Plane(int x, int y): FlyingVehicle(x,y) {
 }
 
 void Plane::printCommand() {
+    // ALGORITHM
+    // Call printCommand() procedure from FlyingVehicle class
     FlyingVehicle::printCommand();
+
+    // Display additional command from Plane class
     std::cout << "0. Stop flying\n" << std::endl;
     std::cout << "Input yout choice: ";
 }
 
-double Plane::lossFuel(int moveX, int moveY) { 
+double Plane::lossFuel(int moveX, int moveY) {
+    // ALGORITHM
+    // Return loss fuel calculation of Plane class
     return 2 * speed * (moveX + moveY) * (1+ 0.1*passanger) * ((25000 - altitude) / 10000);
 }
 
 void Plane::addPassanger() {
+    // DICTIONARY
     int added;
+
+    // ALGORITHM
+    // Asking user input
     std::cout << "Input how many added passanger: "; std::cin >> added;
+    
+    // Invalid input case
     if (added < 0) {
         wrongInput();
         return;
     }
+    // Valid input case
     if ((added + passanger) <= maxSeats) {
         passanger += added;
         std::cout << added << " passanger has been added" << std::endl;
-    } else {
+    } 
+    // Added passanger > rest of the seats case 
+    else {
         std::cout << "Seats are full, with passanger: " << passanger << " and max seats: " << maxSeats << std::endl; 
     }
 }
@@ -365,22 +497,33 @@ void Plane::addPassanger() {
 // =========================================================================================
 
 Helicopter::Helicopter(int x, int y): FlyingVehicle(x,y) {
+    // Construct Helicopter object from FlyingVehicle class
+
     while (true) {
+        // Asking user input
         std::cout << "Please input your number of blades: "; std::cin >> number_of_blade;
+        
+        // Invalid input case
         if (number_of_blade <= 0) {
             std::cout << "Number of engine must be > 0\n" << std::endl;
             std::cout << "Please input your number of blades: "; std::cin >> number_of_blade;
-        } else {
+        } 
+        // Valid input case
+        else {
             break;
         }
     }
 
+    // Display ASCII art
     std::cout << "\n" << std::endl;
     std::cout << "   -----|-----    " << std::endl;
     std::cout << "*>=====[_]L)      " << std::endl;
     std::cout << "      -'-`-       " << std::endl;
+    
+    // Display constuctor message
     std::cout << "Your helicopter with " << number_of_blade << " number of blades has been magically created :D" << std::endl;
 
+    // Define attributes' value
     maxSpeed = 40;
     maxFuel = 5000;
     maxAlti = 4000;
@@ -388,12 +531,18 @@ Helicopter::Helicopter(int x, int y): FlyingVehicle(x,y) {
     vehicleType = "Helicopter";
 }
 
-double Helicopter::lossFuel(int moveX, int moveY) { 
+double Helicopter::lossFuel(int moveX, int moveY) {
+    // ALGORITHM
+    // Return loss fuel calculation of Helicopter class
     return (speed * (1.75) * (moveX + moveY) * (1 + passanger) + (5000 - altitude)/1000); 
 }
 
 void Helicopter::printCommand() {
+    // ALGORITHM
+    // Call printCommand() procedure from FlyingVehicle class
     FlyingVehicle::printCommand();
+
+    // Display additional command from Car class    
     std::cout << "0. Stop flying\n" << std::endl;
     std::cout << "Input yout choice: ";
 }
@@ -401,6 +550,10 @@ void Helicopter::printCommand() {
 // =========================================================================================
 
 FlyingCar::FlyingCar(int x, int y): Car(x,y), FlyingVehicle(x,y) {
+    // Construct FlyingCar object from Car and FlyingVehicle class
+
+    // ALGORITHM
+    // Define attributes' value
     number_of_wheels = 4;
     FlyingVehicle::maxSpeed = 50;
     FlyingVehicle::maxFuel = 2000;
@@ -410,6 +563,8 @@ FlyingCar::FlyingCar(int x, int y): Car(x,y), FlyingVehicle(x,y) {
 }
 
 void FlyingCar::flying(){
+    // ALGORITHM
+    // Check whether FlyingCar is already flying
     if (isFlying) {
         std::cout << "You are already flying" << std::endl;
     } else {
@@ -419,6 +574,8 @@ void FlyingCar::flying(){
 }
 
 void FlyingCar::landing(){
+    // ALGORITHM
+    // Check whether FlyingCar is already on the road
     if (!isFlying) {
         std::cout << "You are already on the road" << std::endl;
     } else {
@@ -429,7 +586,11 @@ void FlyingCar::landing(){
 }
 
 void FlyingCar::printCommand() {
+    // ALGORITHM
+    // Call printCommand() procedure from Vehicle class
     FlyingVehicle::printCommand();
+
+    // Display additional command from FlyingCar class
     std::cout << "9. Start flying" << std::endl;
     std::cout << "10. Use land road" << std::endl;
     std::cout << "11. Use NOS" << std::endl;
@@ -437,12 +598,17 @@ void FlyingCar::printCommand() {
     std::cout << "Input yout choice: ";
 }
 
-double FlyingCar::lossFuel(int moveX, int moveY) { 
+double FlyingCar::lossFuel(int moveX, int moveY) {
+    // ALGORITHM
+    // Return loss fuel calculation of Car class
     return (FlyingVehicle::speed * (1.5) * (moveX + moveY) * (0.75 * FlyingVehicle::passanger)) * (1 - (altitude/(2*maxAlti)));     
 }
 
 void FlyingCar::setAltitude(){
+    // ALGORITHM
+    // Check whether FlyingCar is already flying
     if (isFlying) {
+        // Call setAltitude() procedure from FlyingVehicle class
         FlyingVehicle::setAltitude();
     } else {
         std::cout << "You must start flying first" << std::endl;
@@ -452,5 +618,6 @@ void FlyingCar::setAltitude(){
 // =========================================================================================
 
 void wrongInput(){
+    // Display invalid input message
     std::cout << "Your input is invalid!" << std::endl;
 }
